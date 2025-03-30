42a356eb6568bbcaf51c00d0817e1795d08a7f23
masaru gotou <masaru.gotou@gmail.com>
2025-03-30 10:13:21 +0900

    clean: Firebaseの設定を別ファイルや環境変数に抽出。ファイル名のハイフンをアンダースコアに変更。 Fixes #79

---
+++ firebase_config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDOFYIn_PQSDHRgdjaSIk3uSK50UeYFyIk",
+    authDomain: "my-web-game-f7504.firebaseapp.com",
+    databaseURL: "https://my-web-game-f7504-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "my-web-game-f7504",
+    storageBucket: "my-web-game-f7504.firebasestorage.app",
+    messagingSenderId: "855871162021",
+    appId: "1:855871162021:web:29fadb3c0d20ca4f7a2f4c",
+};
