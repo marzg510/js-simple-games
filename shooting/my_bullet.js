10aff60515b4a4f47e20921fab0ee2670b7814e3
Masaru Gotou <masaru.gotou@gmail.com>
2025-04-27 12:07:31 +0000

    弾が動く Fixes #130

--- shooting/my_bullet.js
+++ shooting/my_bullet.js
@@ -3,17 +3,15 @@ export class MyBullet {
         this.x = x; // 弾のx座標
         this.y = y; // 弾のy座標
         this.width = 3; // 弾の幅
-        this.height = 10; // 弾の高さ
+        this.height = 30; // 弾の高さ
         this.speed = speed; // 弾の速度
         this.isActive = true; // 弾がアクティブかどうか
     }
 
     update() {
-        // いったんは、何もしない。
-        this.isActive = true; // 弾をアクティブにする
-        // this.y -= this.speed; // 弾を上方向に移動
-        // if (this.y < 0) {
-            // this.isActive = false; // 画面外に出たら非アクティブにする
-        // }
+        this.y -= this.speed; // 弾を上方向に移動
+        if (this.y + this.height < 0) {
+            this.isActive = false; // 画面外に出たら非アクティブにする
+        }
     }
 }
