3848cea135ae75c4252bd10ae2dba313d40b58e6
Masaru Gotou <masaru.gotou@gmail.com>
2025-05-01 14:43:22 +0000

    EnemyRendererの重複の排除

--- shooting/my_ship_renderer.js
+++ shooting/my_ship_renderer.js
@@ -16,32 +16,10 @@ export class MyShipRenderer {
             this.height
         );
         // コリジョンエリアを描画
+        this.ctx.save(); // 現在の状態を保存
         this.ctx.strokeStyle = "red";
         this.ctx.lineWidth = 2;
         this.ctx.strokeRect(myShip.x, myShip.y, myShip.width, myShip.height);
         this.ctx.restore(); // 状態を復元
     }
-}
-
-export class EnemyRenderer {
-  constructor(ctx, enemyImageSrc, width, height) {
-      this.ctx = ctx;
-      this.enemyImage = new Image();
-      this.enemyImage.src = enemyImageSrc;
-      this.width = width;
-      this.height = height;
-  }
-
-  render(enemy) {
-      this.ctx.drawImage(
-          this.enemyImage,
-          enemy.x,
-          enemy.y,
-          this.width,
-          this.height
-      );
-      this.ctx.strokeStyle = "red";
-      this.ctx.lineWidth = 2;
-      this.ctx.strokeRect(enemy.x, enemy.y, enemy.width, enemy.height);
-  }
 }
